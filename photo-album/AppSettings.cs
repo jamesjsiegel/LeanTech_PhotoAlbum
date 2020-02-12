6ab876d4bb684972e3b40a3b89469432e2ec6134
James Siegel <jamesjsiegel@gmail.com>
2020-02-11 20:47:24 -0600

    Initial commit

---
+++ photo-album/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace photo_album
+{
+    public class AppSettings
+    {
+        private static IConfiguration _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+        public static string PhotoServiceUri { get; } = _config["photoServiceUrl"];
+    }
+}
